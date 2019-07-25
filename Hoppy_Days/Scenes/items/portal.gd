9a344f36dd0fe7b9a7d9a486f0ab808e47a934de
kruglinski <kruglinski@gmail.com>
2019-07-25 23:08:05 +0800

    Make player transporting between portals

--- Hoppy_Days/Scenes/items/portal.gd
+++ Hoppy_Days/Scenes/items/portal.gd
@@ -1,9 +1,25 @@
 extends Area2D
 
+export var child_portal = false
+
 onready var anim = get_node("anim")
 
+var other_portal
+var transporting = false
+
 func _ready():
+
+	other_portal = get_node("peer")
+	if not other_portal:
+		other_portal = get_parent()
+
 	anim.play("portal_blink")
 
 func _on_portal_body_enter( body ):
-	body.enter_portal(self)
+	if not transporting:
+		other_portal.transporting = true
+		body.set_pos(other_portal.get_global_pos())
+		body.enter_portal(self)
+
+func _on_portal_body_exit( body ):
+	transporting = false
