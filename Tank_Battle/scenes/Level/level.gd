17b3c4073428cf6dcb39480a028417047d10f435
kruglinski <kruglinski@gmail.com>
2019-09-27 20:12:54 +0800

    remove unfinished game

--- Tank_Battle/scenes/Level/level.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-onready var Controller = get_node("Controller")
-onready var Player = get_node("Player")
-
-func _ready():
-	Controller.player = Player
--- Tank_Battle/scenes/Player/Player.gd
+++
@@ -1,4 +0,0 @@
-extends "res://scenes/Tank/Tank.gd"
-
-func gun_fire():
-	print("gun fire")
